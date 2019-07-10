9d72d1f7072cbf02deba53e2a7de68aff0b44b6c
WaelFa <wael.fadhel93@gmail.com>
2019-07-10 13:09:09 +0100

    last commit

--- src/UserDetails.js
+++ src/UserDetails.js
@@ -7,13 +7,14 @@ export default class UserDetails extends Component {
     
         this.state = {
             posts: [],
+            loading: true,
             name: props.match.params.username,  
         }
     }
     componentWillMount() {
         fetch('https://randomuser.me/api/?results=10&seed=emiketic')
             .then(res => res.json())
-            .then(data => this.setState({ posts: data.results }))
+            .then(data => this.setState({ posts: data.results, loading: false }))
         
         
     }
@@ -23,21 +24,26 @@ export default class UserDetails extends Component {
     
     
     render() {
+
+        const { loading } = this.state;
+        if (loading) {
+            return <div>Loading ...</div>
+        }
         const element = this.state.posts.filter(el => el.name.first === this.state.name)[0];
         console.log(element)
-       
     return (
             <div>
                 <center>
                     <h1>User Details</h1>
-                    <br />
-                    <h3>name : {}</h3>
-                    <h3>email : {}</h3>
-                    <h3>phone : {}</h3>
-                    <h3>gender : {}</h3>
-                    <h3>location : {}</h3>
-                    <h3>picture : {}</h3>
-                    <Link to="/" ><button>Back</button></Link>
+                    <br/>
+                    <h3>name : {element.name.first}</h3>
+                    <h3>email : {element.email}</h3>
+                    <h3>phone : {element.phone}</h3>
+                    <h3>gender : {element.gender}</h3>
+                    <h3>location : {element.location.city}</h3>
+                <h3>picture : {element.picture.large}</h3>
+                <img src={element.picture.large} />
+                <Link to="/" ><button>Back</button></Link>
                 </center>
                 
             </div>
